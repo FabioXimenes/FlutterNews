b1e73fece039f5b14078c359dbf042459d35f19a
FabioXimenes <fabio.ximenes@lapisco.ifce.edu.br>
2021-01-04 22:28:29 -0300

    Add url launcher.

--- lib/app/shared/components/custom_modal_bottom_sheet.dart
+++ lib/app/shared/components/custom_modal_bottom_sheet.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_news_app/app/shared/models/article_model.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class CustomModalButtonSheet extends StatelessWidget {
   final ArticleModel article;
@@ -12,53 +13,61 @@ class CustomModalButtonSheet extends StatelessWidget {
     final Size _size = MediaQuery.of(context).size;
     final DateTime _date = DateTime.tryParse(article.publishedAt);
 
-    return Container(
-      padding: EdgeInsets.all(20),
-      child: ListView(
-        children: [
-          SizedBox(height: 5),
-          Text(
-            article.title,
-            style: Theme.of(context).textTheme.headline4,
-          ),
-          SizedBox(height: 5),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text('by ${article.author}',
-                  style: Theme.of(context)
-                      .textTheme
-                      .subtitle1
-                      .copyWith(color: Colors.grey)),
-              Text('${_date.day}/${_date.month}/${_date.year}',
-                  style: Theme.of(context)
-                      .textTheme
-                      .subtitle1
-                      .copyWith(color: Colors.grey))
-            ],
-          ),
-          SizedBox(height: 10),
-          Container(
-            height: _size.height * 0.25,
-            width: _size.width * 0.9,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: NetworkImage(article.urlToImage),
-              ),
-              borderRadius: BorderRadius.circular(20),
+    return GestureDetector(
+      onTap: () => _launchURL(article.url),
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: ListView(
+          children: [
+            SizedBox(height: 5),
+            Text(
+              article.title,
+              style: Theme.of(context).textTheme.headline4,
             ),
-          ),
-          SizedBox(height: 10),
-          Expanded(
-            child: Text(
+            SizedBox(height: 5),
+            Text(
+              '${_date.day}/${_date.month}/${_date.year}',
+              style: Theme.of(context)
+                  .textTheme
+                  .subtitle1
+                  .copyWith(color: Colors.grey),
+            ),
+            Text(
+              'by ${article.author}',
+              style: Theme.of(context)
+                  .textTheme
+                  .subtitle1
+                  .copyWith(color: Colors.grey),
+            ),
+            SizedBox(height: 10),
+            Container(
+              height: _size.height * 0.25,
+              width: _size.width * 0.9,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(article.urlToImage),
+                ),
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
               article.description,
-              style: Theme.of(context).textTheme.subtitle1,
+              style: Theme.of(context).textTheme.subtitle1.copyWith(color: Colors.grey[900]),
               // overflow: TextOverflow.ellipsis,
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
+  }
+}
+
+_launchURL(url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
 }
